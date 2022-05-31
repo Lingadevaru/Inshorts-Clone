ec983e62e8f1311d9a197a430520ce78edd29957
Lingadevaru <lingadevaruc5@gmail.com>
2022-05-31 22:48:08 +0530

    Initial Commit

---
+++ src/data/category.js
@@ -0,0 +1,14 @@
+const categories = [
+    "breaking-news",
+    "world",
+    "nation",
+    "business",
+    "technology",
+    "entertainment",
+    "sports",
+    "science",
+    "health",
+  ];
+  
+  export default categories;
+  
---
+++ src/data/config.js
@@ -0,0 +1,3 @@
+//const apikey ="e2fb143850514ef385cc509f9e54e067"; //NewsApi
+const apikey = "5b1be87ef86406f204085bd582096455"
+export default apikey;
